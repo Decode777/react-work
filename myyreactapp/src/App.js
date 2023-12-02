03857e2ff34779448f0ddfee31d19eb03baec1b2
Decode777 <vaishnavis979@gmail.com>
2023-12-02 21:25:47 +0530

    card app

--- myyreactapp/src/App.js
+++ myyreactapp/src/App.js
@@ -3,26 +3,42 @@
 //import { MathsComponent } from './components/Maths.jsx';
 //import MyComponent from './components/food.jsx';
 //import { GreetingsComponent } from './components/reactStylePrac.jsx';
-import Header from './components/Header.jsx';
-import Note from './components/Note.jsx';
-import Footer from './components/Footer.jsx';
-import "./components/styles.css";
+//import Header from './components/Header.jsx';
+//import Note from './components/Note.jsx';
+//import Footer from './components/Footer.jsx';
+//import "./components/styles.css";
+import  Card  from './components/Card.jsx';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      {/* <MyComponent /> */}
-        {/* <GreetingsComponent /> */}
-        {/* <List /> */}
-        {/* <List /> */}
-        {/* <MathsComponent /> */}
-       
+    <div>
+      {/* 
+        <MyComponent />
+        <GreetingsComponent />
+        <List />
+        <List />
+        <MathsComponent />
+      */}
+      {/* 
         <Header />
         <Note />
         <Footer />
-      </header>
+      */}
+      <h1>My Contacts</h1>
+      <Card
+        name="John Doe"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        tel="123-456-7890"
+        email="john@example.com"
+      />
+      <Card
+      name="Beyonce"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+    />
+      {/* Add more Card components with different props as needed */}
     </div>
   );
 }
