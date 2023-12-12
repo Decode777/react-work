a5a0f64857be1884c44240c3fa1a3b0a7c433a74
Decode777 <vaishnavis979@gmail.com>
2023-12-12 19:25:09 +0530

    .

--- myyreactapp/src/App.js
+++ myyreactapp/src/App.js
@@ -7,8 +7,10 @@
 //import Note from './components/Note.jsx';
 //import Footer from './components/Footer.jsx';
 //import "./components/styles.css";
+import React from 'react';
 import  Card  from './components/Card.jsx';
-
+import contacts from './contacts.js';
+import Avatar from './components/Avatar.jsx';
 
 function App() {
   return (
@@ -25,7 +27,7 @@ function App() {
         <Note />
         <Footer />
       */}
-      <h1>My Contacts</h1>
+      {/* <h1>My Contacts</h1>
       <Card
         name="John Doe"
         img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
@@ -37,8 +39,30 @@ function App() {
       img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
       tel="+123 456 789"
       email="b@beyonce.com"
-    />
+    /> */}
       {/* Add more Card components with different props as needed */}
+      
+      <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://pbs.twimg.com/profile_images/744849215675838464/IH0FNIXk.jpg" />
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      />
+
     </div>
   );
 }
--- myyreactapp/src/components/Card.jsx
+++ myyreactapp/src/components/Card.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+// import React from "react";
 
+
+// function Card(props) {
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <img src={props.img} alt="avatar_img" />
+//       <p>{props.tel}</p>
+//       <p>{props.email}</p>
+//     </div>
+//   );
+// }
+
+// export default Card;
+
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
 
 function Card(props) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatar_img" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
+      </div>
     </div>
   );
 }
